cab8b2a5045e7090f2189e5f88217098ae6e69fb
Rajasimha Reddy <rajasimha5599@gmail.com>
2020-02-06 14:41:37 +0530

    setup databse

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require("http");
-
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
 const hostname = "127.0.0.1";
 const port = 3000;
 
@@ -11,4 +12,23 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+
+// Database Name
+const dbName = "myproject";
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
 });
